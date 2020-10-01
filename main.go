58c1f8b471f954151770700c2fba6057e34b8017
suotas <accf8b33@gmail.com>
2020-10-01 22:11:35 +0900

    implement short moving average

--- main.go
+++ main.go
@@ -9,12 +9,28 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const (
+	ohlcvIdx = 0
+	candlestickOpenPriceIdx = 0
+	candlestickClosePriceIdx = 3
+)
+
 func getCandlesticks(client *bitbank.Client) {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	candlesticks, _ := client.GetCandlesticks(ctx, "btc_jpy", "1day", "2020")
-	candlesticksData := candlesticks.Data.Candlesticks
-	for i, v := range candlesticksData {
-		fmt.Println(i, v)
+	ohlcv := candlesticks.Data.Candlesticks[ohlcvIdx].Ohlcv
+
+	var temp = 0
+	if (len(ohlcv) > 75) {
+		calcTemp := ohlcv[len(ohlcv)-5:]
+		for _, v := range calcTemp {
+			closePriceInt, _ := v[candlestickClosePriceIdx].Int64()
+			temp += int(closePriceInt)
+		}
+		temp /= 5
+		fmt.Println(temp)
+	} else {
+		fmt.Println("Not enough data.");
 	}
 }
 
@@ -22,5 +38,4 @@ func main() {
 	godotenv.Load(".env")
 	client, _ := bitbank.NewClient(os.Getenv("BITBANK_API_KEY"), os.Getenv("BITBANK_SECRET"), nil)
 	getCandlesticks(client)
-	time.Sleep(time.Second * 1)
 }
