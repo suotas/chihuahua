b31f7f0e8b47c9b273c486dc8f5804f765ca476f
suotas <accf8b33@gmail.com>
2020-11-07 22:27:15 +0900

    add order model

---
+++ domain/model/orders.go
@@ -0,0 +1,19 @@
+package model
+
+type Orders struct {
+	Order []*Order
+}
+
+type Order struct {
+	OrderID string			`json:"order_id,omitempty"`
+	Pair string				`json:"pair,omitempty"`
+	Side string				`json:"side,omitempty"`
+	Type string				`json:"type,omitempty"`
+	StartAmount string		`json:"start_amount,omitempty"`
+	RemainingAmount string	`json:"remaining_amount,omitempty"`
+	ExecutedAmount string	`json:"executed_amount,omitempty"`
+	Price string			`json:"price,omitempty"`
+	AveragePrice string		`json:"average_price,omitempty"`
+	OrderedAt int64			`json:"ordered_at,omitempty"`
+	Status string			`json:"status,omitempty"`
+}
